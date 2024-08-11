ed7905921d3ab4f0618ec88c307d93dfeb97b27c
sanmiguelight <mlgvalenzuela@yahoo.com>
2024-08-11 21:05:14 +0800

    Post-render shutdown v2. This script now sends an email notification once the rendered file is detected and before shutting down blender.exe and the PC. The sender email, receiver email, and the app password is hardcoded, and the only user input would still be the expected filepath of the rendered file.

--- Render_Shutdown.py
+++ Render_Shutdown.py
@@ -3,6 +3,7 @@ import time
 import subprocess
 from playsound import playsound
 from pathlib import Path
+import smtplib
 
 # Function that closes Blender and shutdowns the PC
 def shutdown():
@@ -14,43 +15,54 @@ def shutdown():
         # Attempt to shut down the PC
         subprocess.call(["shutdown", "/s", "/t", "0"])
         print("Shutting down...")
-        time.sleep(5)
 
-    except Exception:  # If an error occurs, this is printed
+    except Exception:
         print(f"An error occured: {Exception}")
 
+#Function that sends the notification email once the render is finished
+def send_email(filepath):
+    sender_email = "sender@gmail.com"
+    receiver_email = "receiver@gmail.com"
+    app_password = "enter app password here"
 
-def main():
-    print("POST-RENDER SHUTDOWN\n")
-    filepath = input("Enter the expected complete filepath of the rendered image: ")
+    email_content = f"Subject: RENDER FINISHED\n\nThe rendered image is saved in {filepath}"
 
-    while True:
-        if os.path.exists(filepath):  # Runs when True is returned
-            print("\nRender is complete!")
-            playsound(str(Path(__file__).parent / 'homecoming.mp3'))
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(sender_email, app_password)
+    server.sendmail(sender_email, receiver_email, email_content)
+    print ("Notification mail sent!\n")
 
-            # 30 second countdown before shutdown
-            for remaining in range(30, 0, -1):
-                print(
-                    f"\rShutting down in {remaining} seconds... ", end="", flush=True)
-                time.sleep(1)
-            break
+print("POST-RENDER SHUTDOWN\n")
+filepath = input("Enter the expected complete filepath of the rendered image: ")
 
-        else:
-            print("\nRendering is still ongoing")
-            # 2.5 minute countdown
-            for remaining in range(150, 0, -1):
-                # Divide "remaining" to minutes and seconds
-                minutes, seconds = divmod(remaining, 60)
+while True:
+    if os.path.exists(filepath):  # Runs when True is returned
+        print("\nRender is complete!")
+        playsound(str(Path(__file__).parent / 'homecoming.mp3'))
 
-                # Time formatting
-                if minutes > 0:
-                    time_format = f"{minutes}m {seconds}s"
-                else:
-                    time_format = f"{seconds}s"
+        send_email(filepath)
 
-                print(f"\rRe-checking in {time_format}...", end="", flush=True)
-                time.sleep(1)
+        # 30 second countdown before shutdown
+        for remaining in range(30, 0, -1):
+            print(f"\rShutting down in {remaining} seconds... ", end="", flush=True)
+            time.sleep(1)
+        break
 
-main()
+    else:
+        print("\nRendering is still ongoing")
+        # 2.5 minute countdown
+        for remaining in range(150, 0, -1):
+            # Divide "remaining" to minutes and seconds
+            minutes, seconds = divmod(remaining, 60)
+
+            # Time formatting
+            if minutes > 0:
+                time_format = f"{minutes}m {seconds}s"
+            else:
+                time_format = f"{seconds}s"
+
+            print(f"\rRe-checking in {time_format}...", end="", flush=True)
+            time.sleep(1)
+
 shutdown() #Initiate the shutdown of Blender and the PC
